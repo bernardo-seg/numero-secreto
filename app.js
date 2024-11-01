5587809240f990029c691f3635d6ee3115debada
Bernardo Segovia Guimarães <bernardoseg@gmail.com.br>
2024-11-01 09:04:38 -0300

    resolvido conflido para 50 dificuldade

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 let listaDeNumerosSorteados = [];
-<<<<<<< HEAD
 let numeroLimite = 50;
-=======
-let numeroLimite = 10;
->>>>>>> parent of 15115cd (alterado a dificulade do jogo para 100)
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
